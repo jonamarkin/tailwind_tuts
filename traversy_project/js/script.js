acbfcdd58ded1c7fda6e6130cfd1af7d631f9bef
Jonathan Ato Markin <jona10711@gmail.com>
2024-04-14 18:37:05 +0200

    Hamburger menu added

---
+++ traversy_project/js/script.js
@@ -0,0 +1,8 @@
+const btn = document.getElementById('menu-btn');
+const nav = document.getElementById('menu');
+
+btn.addEventListener('click', () => {
+    nav.classList.toggle('open');
+    nav.classList.toggle('flex');
+    nav.classList.toggle('hidden');
+});
